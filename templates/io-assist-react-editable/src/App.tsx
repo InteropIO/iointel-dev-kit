43f4e9214fdd27e831f2e275b5a8f53252b57b70
Kalin Kostov <k.kostov.eu@gmail.com>
2026-07-30 15:39:37 +0300

    added react editable shell
    Signed-off-by: Kalin Kostov <k.kostov.eu@gmail.com>

---
+++ templates/io-assist-react-editable/src/App.tsx
@@ -0,0 +1,62 @@
+import { useMemo } from 'react'
+import IOBrowser from '@interopio/browser'
+import {
+  IoAssist,
+  type IoAssistDynamicConfig,
+  type IoAssistStaticConfig,
+} from './io-assist/something'
+import IOWorkspaces from '@interopio/workspaces-api'
+import './App.css'
+
+// this must be defined;
+const AGENT_SERVER_URL = '';
+
+const createIOConnect: typeof IOBrowser = async (config) => {
+  const io = await IOBrowser(config);
+
+  (window as any).io = io;
+
+  return io
+}
+
+const staticConfig: IoAssistStaticConfig = {
+  connectConfig: {
+    browser: {
+      factory: createIOConnect,
+      config: {
+        libraries: [IOWorkspaces],
+        modals: {
+          dialogs: {
+            enabled: true,
+          },
+        },
+      },
+    },
+  },
+  defaultAgentName: 'io-agent',
+  aiWebConfig: {
+    agentServer: {
+      baseUrl: AGENT_SERVER_URL,
+    },
+  },
+}
+
+function App() {
+  const dynamicConfig = useMemo<IoAssistDynamicConfig>(
+    () => ({
+      user: {
+        id: 'acme-advisor',
+        name: 'ACME Advisor',
+      },
+    }),
+    [],
+  )
+
+  return (
+    <main className="assist-shell">
+      <IoAssist staticConfig={staticConfig} dynamicConfig={dynamicConfig} />
+    </main>
+  )
+}
+
+export default App
